ebc67a124fd83990379c76bed907e3716e2aa75a
Martin <martin.dinahet@proton.me>
2025-06-24 16:53:42 +0200

    chore(all): initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use server";
+
+const HomePage: React.FC = async () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+export default HomePage;
